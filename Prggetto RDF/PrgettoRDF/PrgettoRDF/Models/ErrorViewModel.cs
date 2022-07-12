363558cf72a0a58d3f683ec3fd3adfa136296c3f
Abro01 <93769284+Abro01@users.noreply.github.com>
2022-07-12 12:37:54 +0200

    Start

---
+++ Prggetto RDF/PrgettoRDF/PrgettoRDF/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PrgettoRDF.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
