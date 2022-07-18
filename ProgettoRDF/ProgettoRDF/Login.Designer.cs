483e1f2d8c58f440c7c8302b0012f02f4fd83234
Alessio <panizzieri24@gmail.com>
2022-07-18 17:01:38 +0200

    dbconnection

--- ProgettoRDF/ProgettoRDF/Login.Designer.cs
+++ ProgettoRDF/ProgettoRDF/Login.Designer.cs
@@ -29,35 +29,37 @@
         private void InitializeComponent()
         {
             this.Accedi = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textEmail = new System.Windows.Forms.TextBox();
+            this.textPassword = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // Accedi
             // 
-            this.Accedi.Location = new System.Drawing.Point(100, 145);
+            this.Accedi.Location = new System.Drawing.Point(130, 142);
             this.Accedi.Name = "Accedi";
             this.Accedi.Size = new System.Drawing.Size(102, 53);
             this.Accedi.TabIndex = 0;
             this.Accedi.Text = "Login";
             this.Accedi.UseVisualStyleBackColor = true;
-            this.Accedi.Click += new System.EventHandler(this.button1_Click);
+            this.Accedi.Click += new System.EventHandler(this.Accedi_Click);
             // 
-            // textBox1
+            // textEmail
             // 
-            this.textBox1.Location = new System.Drawing.Point(187, 30);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 1;
+            this.textEmail.Location = new System.Drawing.Point(187, 30);
+            this.textEmail.Name = "textEmail";
+            this.textEmail.Size = new System.Drawing.Size(100, 20);
+            this.textEmail.TabIndex = 1;
             // 
-            // textBox2
+            // textPassword
             // 
-            this.textBox2.Location = new System.Drawing.Point(187, 71);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 2;
+            this.textPassword.Location = new System.Drawing.Point(187, 71);
+            this.textPassword.Name = "textPassword";
+            this.textPassword.Size = new System.Drawing.Size(100, 20);
+            this.textPassword.TabIndex = 2;
             // 
             // label1
             // 
@@ -77,19 +79,29 @@
             this.label2.TabIndex = 4;
             this.label2.Text = "password";
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(444, 30);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(917, 150);
+            this.dataGridView1.TabIndex = 5;
+            // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(442, 207);
+            this.ClientSize = new System.Drawing.Size(1419, 367);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textPassword);
+            this.Controls.Add(this.textEmail);
             this.Controls.Add(this.Accedi);
             this.Name = "Login";
             this.Text = "Home";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -98,10 +110,11 @@
         #endregion
 
         private System.Windows.Forms.Button Accedi;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textEmail;
+        private System.Windows.Forms.TextBox textPassword;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- ProgettoRDF/ProgettoRDF/Login.cs
+++ ProgettoRDF/ProgettoRDF/Login.cs
@@ -7,15 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using MySql.Data.MySqlClient;
 
 namespace ProgettoRDF
 {
     public partial class Login : Form
     {
+
+        myDBconnection con = new myDBconnection();
+        MySqlCommand command;
+        MySqlDataAdapter da;
+        DataTable dt;
         public Login()
         {
             InitializeComponent();
+            con.Connect();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -23,12 +29,28 @@ namespace ProgettoRDF
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Accedi_Click(object sender, EventArgs e)
         {
+            string emailIN, passwordIN;
 
+            emailIN = textEmail.Text;
+            passwordIN = textPassword.Text;
+
+            try
+            {
+                con.cn.Open();
+                command = new MySqlCommand("Select * from utenti", con.cn);
+                command.ExecuteNonQuery();
+                dt = new DataTable();
+                da = new MySqlDataAdapter(command);
+                da.Fill(dt);
+                dataGridView1.DataSource = dt.DefaultView;
+                con.cn.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
-
-        
-
     }
 }
